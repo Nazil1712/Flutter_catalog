563a01f7d29d595e7edbdd02231433e085fab6e3
Nazil1712 <ndhalwala@gmail.com>
2024-04-28 14:46:37 +0530

    Dark Theme

--- lib/main.dart
+++ lib/main.dart
@@ -10,20 +10,13 @@ void main()
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    bringvegetables(bag: true, rupees: 4580);
-    bringvegetables(rupees: 4580);
-    double Pi = 3.14;
-    bool flag = true;
-    num temp = 30.15; // Can accept any number (double,int)
-    const PI = 3.14; // We can't change
-    final list = [1,2,3,45,5,68]; // We can Add Or remove element inside this
-
     return MaterialApp(
       home: HomePage(),
+      themeMode: ThemeMode.dark,
+      darkTheme: ThemeData(
+        brightness: Brightness.dark
+      ),
     );
   }
-
-  // bringvegetables({bool bag=false, int rupees=100}) {} // Here Bag is optional
-  bringvegetables({@required bool bag=false, int rupees=100}) {} // Parameter bag is required
 }
 
