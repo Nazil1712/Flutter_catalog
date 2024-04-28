ba0c0ab6dfea8ca79141218ea3266937895dbe18
Nazil1712 <ndhalwala@gmail.com>
2024-04-28 12:17:11 +0530

    basics of flutter-dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,21 @@ void main()
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
+    int days = 70;
+    double tt = 45.2;
+    String name = "Hello Bro";
+    double Pi = 3.14;
+    bool flag = true;
+    num temp = 30.15; // Can accept any number (double,int)
+    const PI = 3.14; // We can't change
+    final list = [1,2,3,45,5,68]; // We can Add Or remove element inside this
+
     return MaterialApp(
       home: Material(
         child: Center(
-          child: Text("Welcome to 30 days of Flutter course!!!"),
+          child: Text("Welcome to $days days of Flutter course!!! $name"),
         ),
       ),
     );
